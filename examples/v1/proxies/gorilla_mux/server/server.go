91c784db53a7aeab0c8caa7afab45833554432c4
wusticality <1028792+wusticality@users.noreply.github.com>
2022-11-09 17:38:19 -0600

    Simplify example web server; move CRUD ops to tests/

--- examples/v1/proxies/gorilla_mux/server/server.go
+++ examples/v1/proxies/gorilla_mux/server/server.go
@@ -8,25 +8,43 @@ import (
 
 	api "github.com/styrainc/styra-run-sdk-go/api/v1"
 	rbac "github.com/styrainc/styra-run-sdk-go/rbac/v1"
+	"github.com/styrainc/styra-run-sdk-go/rbac/v1/proxy/list_user_bindings"
 	"github.com/styrainc/styra-run-sdk-go/types"
 
 	"github.com/styrainc/styra-run-sdk-go/api/v1/proxy/batch_query"
 	"github.com/styrainc/styra-run-sdk-go/api/v1/proxy/check"
-	"github.com/styrainc/styra-run-sdk-go/api/v1/proxy/delete_data"
-	"github.com/styrainc/styra-run-sdk-go/api/v1/proxy/get_data"
-	"github.com/styrainc/styra-run-sdk-go/api/v1/proxy/put_data"
 	"github.com/styrainc/styra-run-sdk-go/api/v1/proxy/query"
-	"github.com/styrainc/styra-run-sdk-go/api/v1/proxy/shared"
+	ashared "github.com/styrainc/styra-run-sdk-go/api/v1/proxy/shared"
 	"github.com/styrainc/styra-run-sdk-go/rbac/v1/proxy/delete_user_binding"
 	"github.com/styrainc/styra-run-sdk-go/rbac/v1/proxy/get_roles"
 	"github.com/styrainc/styra-run-sdk-go/rbac/v1/proxy/get_user_binding"
 	"github.com/styrainc/styra-run-sdk-go/rbac/v1/proxy/list_user_bindings_all"
 	"github.com/styrainc/styra-run-sdk-go/rbac/v1/proxy/put_user_binding"
+	rshared "github.com/styrainc/styra-run-sdk-go/rbac/v1/proxy/shared"
 )
 
 const (
 	tenant  = "acmecorp"
 	subject = "alice"
+)
+
+var (
+	users = []*rbac.User{
+		{Id: "alice"},
+		{Id: "bob"},
+		{Id: "bryan"},
+		{Id: "cesar"},
+		{Id: "emily"},
+		{Id: "gary"},
+		{Id: "henry"},
+		{Id: "kevin"},
+		{Id: "lynn"},
+		{Id: "jiri"},
+		{Id: "larry"},
+		{Id: "alan"},
+		{Id: "terence"},
+		{Id: "eckhart"},
+	}
 )
 
 type WebServerSettings struct {
@@ -65,30 +83,6 @@ func (w *webServer) Listen() error {
 
 	// Client handlers.
 	{
-		// Get data.
-		install(get_data.New(
-			&get_data.Settings{
-				Client:  w.settings.Client,
-				GetPath: key("path"),
-			}), "/data/{path:.*}",
-		)
-
-		// Put data.
-		install(put_data.New(
-			&put_data.Settings{
-				Client:  w.settings.Client,
-				GetPath: key("path"),
-			}), "/data/{path:.*}",
-		)
-
-		// Delete data.
-		install(delete_data.New(
-			&delete_data.Settings{
-				Client:  w.settings.Client,
-				GetPath: key("path"),
-			}), "/data/{path:.*}",
-		)
-
 		// Query.
 		install(query.New(
 			&query.Settings{
@@ -110,7 +104,7 @@ func (w *webServer) Listen() error {
 			&batch_query.Settings{
 				Client:        w.settings.Client,
 				GetSession:    getSession,
-				OnModifyInput: shared.DefaultOnModifyInput(),
+				OnModifyInput: ashared.DefaultOnModifyInput(),
 			}), "/batch_query",
 		)
 	}
@@ -140,12 +134,13 @@ func (w *webServer) Listen() error {
 		)
 
 		// List user bindings.
-		//install(list_user_bindings.New(
-		//	&list_user_bindings.Settings{
-		//		Rbac:       myRbac,
-		//		GetSession: getSession,
-		//	}), "/user_bindings_all",
-		//)
+		install(list_user_bindings.New(
+			&list_user_bindings.Settings{
+				Rbac:       myRbac,
+				GetSession: getSession,
+				GetUsers:   rshared.DefaultGetUsers(users, 3),
+			}), "/user_bindings",
+		)
 
 		// Get user binding.
 		install(get_user_binding.New(
