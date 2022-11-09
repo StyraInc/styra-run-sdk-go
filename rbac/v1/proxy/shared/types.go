4b7fdefbf916cf2fdce2c097cbb42849f2818f1a
wusticality <1028792+wusticality@users.noreply.github.com>
2022-11-09 13:06:09 -0600

    Rename OnTouch -> OnBeforeAccess

--- rbac/v1/proxy/shared/types.go
+++ rbac/v1/proxy/shared/types.go
@@ -2,4 +2,4 @@ package shared
 
 import rbac "github.com/styrainc/styra-run-sdk-go/rbac/v1"
 
-type OnUserBindingTouched func(user *rbac.User) (int, error)
+type OnBeforeAccess func(user *rbac.User) (int, error)
